05acd1e9acb94295f7e31179506aca9dfacf7230
Shahzain Haider <72924167+shahzain110@users.noreply.github.com>
2022-09-08 19:16:22 +0500

    Update routes.py

--- blue/api/routes.py
+++ blue/api/routes.py
@@ -34,13 +34,13 @@ class intro_to_API(Resource):
             f.write(video_01)
             f.close()
             #
-            # video_02 = request.files['vid2'].read()
-            # # print("Type: ------------------->", type(video_02))
-            # video_02_path = '/home/shahzain/Documents/GitHub/PoseEst_dep/PoseEst/resouces/2.mp4'
-            # f = open(video_02_path, 'wb')
-            # sources.append(video_02_path)
-            # f.write(video_02)
-            # f.close()
+            video_02 = request.files['vid2'].read()
+            # print("Type: ------------------->", type(video_02))
+            video_02_path = '/home/shahzain/Documents/GitHub/PoseEst_dep/PoseEst/resouces/2.mp4'
+            f = open(video_02_path, 'wb')
+            sources.append(video_02_path)
+            f.write(video_02)
+            f.close()
             # match_perc = basic_function(sources)
             dic = {"status": 200, "msg": "match_perc"}
         except Exception as e:
