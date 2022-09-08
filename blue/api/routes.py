7b2de9e6fc332dfae6171e4f41e8b734ef3537ad
Shahzain Haider <72924167+shahzain110@users.noreply.github.com>
2022-09-08 18:10:21 +0500

    Update routes.py

--- blue/api/routes.py
+++ blue/api/routes.py
@@ -30,8 +30,24 @@ from functions import basic_function
 class intro_to_API(Resource):
     def get(self):
         try:
-            basic_function()
-            dic = {"status":200,"msg":"ok"}
+        sources = []
+            video_01 = request.files['vid1'].read()
+            # print("Type: ------------------->", type(video_01))
+            video_01_path = '/home/shahzain/Documents/GitHub/PoseEst_dep/PoseEst/resouces/1.mp4'
+            sources.append(video_01_path)
+            f = open(video_01_path, 'wb')
+            f.write(video_01)
+            f.close()
+
+            video_02 = request.files['vid2'].read()
+            # print("Type: ------------------->", type(video_02))
+            video_02_path = '/home/shahzain/Documents/GitHub/PoseEst_dep/PoseEst/resouces/2.mp4'
+            f = open(video_02_path, 'wb')
+            sources.append(video_02_path)
+            f.write(video_02)
+            f.close()
+            match_perc = detection(sources)
+            dic = {"status": 200, "msg": match_perc}
             return jsonify(dic)
         except Exception as e:
             dic = {"status":444,"msg":"faliure"}
