d93b9a0c1aac11d6c10daee8e119249c403e897a
Shahzain Haider <72924167+shahzain110@users.noreply.github.com>
2022-09-08 19:10:42 +0500

    last

--- blue/__init__.py
+++ blue/__init__.py
@@ -1,11 +1,9 @@
 from flask import Flask
 from flask_cors import CORS, cross_origin
 from flask_restful import Api,Resource
-from api.routes import mod
+from blue.api.routes import mod
 
 app = Flask(__name__)
 CORS(app)
-
-from blue.api.routes import mod 
 
 app.register_blueprint(mod,url_prefix = '/api')
