02a4be1a8c2bc6dea57f5f52d632b2bebc8fedf7
Shahzain Haider <72924167+shahzain110@users.noreply.github.com>
2022-09-08 18:53:02 +0500

    Update routes.py

--- blue/api/routes.py
+++ blue/api/routes.py
@@ -28,30 +28,30 @@ from functions import basic_function
 
 
 class intro_to_API(Resource):
-    def get(self):
+    def post(self):
         try:
-            sources = []
-            video_01 = request.files['vid1'].read()
-            # print("Type: ------------------->", type(video_01))
-            video_01_path = '/home/shahzain/Documents/GitHub/PoseEst_dep/PoseEst/resouces/1.mp4'
-            sources.append(video_01_path)
-            f = open(video_01_path, 'wb')
-            f.write(video_01)
-            f.close()
-
-            video_02 = request.files['vid2'].read()
-            # print("Type: ------------------->", type(video_02))
-            video_02_path = '/home/shahzain/Documents/GitHub/PoseEst_dep/PoseEst/resouces/2.mp4'
-            f = open(video_02_path, 'wb')
-            sources.append(video_02_path)
-            f.write(video_02)
-            f.close()
-            match_perc = basic_function(sources)
-            dic = {"status": 200, "msg": match_perc}
-            return jsonify(dic)
+            # sources = []
+            # video_01 = request.files['vid1'].read()
+            # # print("Type: ------------------->", type(video_01))
+            # video_01_path = '/home/shahzain/Documents/GitHub/PoseEst_dep/PoseEst/resouces/1.mp4'
+            # sources.append(video_01_path)
+            # f = open(video_01_path, 'wb')
+            # f.write(video_01)
+            # f.close()
+            #
+            # video_02 = request.files['vid2'].read()
+            # # print("Type: ------------------->", type(video_02))
+            # video_02_path = '/home/shahzain/Documents/GitHub/PoseEst_dep/PoseEst/resouces/2.mp4'
+            # f = open(video_02_path, 'wb')
+            # sources.append(video_02_path)
+            # f.write(video_02)
+            # f.close()
+            # match_perc = basic_function(sources)
+            dic = {"status": 200, "msg": "match_perc"}
         except Exception as e:
             dic = {"status":444,"msg":"faliure"}
-            return jsonify(dic)
+        return jsonify(dic)
+
 
 
 api.add_resource(intro_to_API, "/intro_to_API")
