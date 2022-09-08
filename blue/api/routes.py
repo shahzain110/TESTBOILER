1662a0f3b4dff913053f27ee82045ed237f7cf2c
Shahzain Haider <72924167+shahzain110@users.noreply.github.com>
2022-09-08 19:24:33 +0500

    Update routes.py

--- blue/api/routes.py
+++ blue/api/routes.py
@@ -5,7 +5,7 @@ from flask import Blueprint, jsonify, request
 from flask_restful import Api, Resource
 
 ### change the "/" to "\\" if you are windows user ###
-from api.
+# from api.
 
 dirname = os.path.dirname(os.path.abspath(__file__))
 dirname_list = dirname.split("/")[:-1]
