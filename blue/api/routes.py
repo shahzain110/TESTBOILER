afbc8e89c7c46d143cc303a0cc973738754eee53
Shahzain Haider <72924167+shahzain110@users.noreply.github.com>
2022-09-08 19:14:29 +0500

    Update routes.py

--- blue/api/routes.py
+++ blue/api/routes.py
@@ -1,7 +1,7 @@
 import os
 import sys
 
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, request
 from flask_restful import Api, Resource
 
 ### change the "/" to "\\" if you are windows user ###
@@ -25,14 +25,14 @@ api = Api(mod)
 class intro_to_API(Resource):
     def post(self):
         try:
-            # sources = []
-            # video_01 = request.files['vid1'].read()
-            # # print("Type: ------------------->", type(video_01))
-            # video_01_path = '/home/shahzain/Documents/GitHub/PoseEst_dep/PoseEst/resouces/1.mp4'
-            # sources.append(video_01_path)
-            # f = open(video_01_path, 'wb')
-            # f.write(video_01)
-            # f.close()
+            sources = []
+            video_01 = request.files['vid1'].read()
+            # print("Type: ------------------->", type(video_01))
+            video_01_path = '/home/shahzain/Documents/GitHub/PoseEst_dep/PoseEst/resouces/1.mp4'
+            sources.append(video_01_path)
+            f = open(video_01_path, 'wb')
+            f.write(video_01)
+            f.close()
             #
             # video_02 = request.files['vid2'].read()
             # # print("Type: ------------------->", type(video_02))
